8c6c2986968b12e9aa7be18ea11a65fd389740bd
Dimas Salahuddin <dimassalah10@gmail.com>
2024-04-29 23:46:51 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/tatrupiah_si/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tatrupiah_si
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,18 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const ONBOARDING = _Paths.ONBOARDING;
+  static const SPLASH = _Paths.SPLASH;
+  static const AUTH = _Paths.AUTH;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const ONBOARDING = '/onboarding';
+  static const SPLASH = '/splash';
+  static const AUTH = '/auth';
+}
