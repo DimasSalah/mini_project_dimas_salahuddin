4cb8518635bba8e8be11f892d4ef711cdb0a5291
Dimas Salahuddin <dimassalah10@gmail.com>
2024-05-07 14:46:27 +0700

    feat: fetching user in profile view

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -17,104 +17,110 @@ class ProfileView extends GetView<ProfileController> {
   Widget build(BuildContext context) {
     MainController mainController = Get.find<MainController>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProfileView'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 23),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: Get.height * 0.45,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  color: normal, borderRadius: BorderRadius.circular(24)),
-            ),
-            const Gap(20),
-            Obx(
-              () => Text(
-                mainController.name.value,
-                style: medium.copyWith(
-                  fontSize: 36,
-                  color: dark,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: Get.height * 0.45,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: normal,
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                child: Obx(
+                  () => SvgPicture.string(
+                    mainController.icon.value,
+                    fit: BoxFit.fitHeight,
+                    
+                  ),)
+              ),
+              const Gap(20),
+              Obx(
+                () => Text(
+                  mainController.name.value,
+                  style: medium.copyWith(
+                    fontSize: 36,
+                    color: dark,
+                  ),
                 ),
               ),
-            ),
-            const Gap(5),
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(6),
+              const Gap(5),
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      gradient: primary,
+                      shape: BoxShape.circle,
+                    ),
+                    child: SvgPicture.asset(
+                      'assets/icons/time.svg',
+                      height: 18,
+                    ),
+                  ),
+                  const Gap(10),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Bergabung ',
+                          style: regular.copyWith(
+                            fontSize: 14,
+                            color: lightActive,
+                          ),
+                        ),
+                        TextSpan(
+                          text: DateFormat('dd MMMM yyyy').format(
+                              DateTime.parse(mainController.createdAt.value)),
+                          style: regular.copyWith(
+                            fontSize: 14,
+                            color: dark,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(35),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 11),
                   decoration: BoxDecoration(
-                    gradient: primary,
-                    shape: BoxShape.circle,
+                    color: dark,
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  child: SvgPicture.asset(
-                    'assets/icons/time.svg',
-                    height: 18,
-                  ),
-                ),
-                const Gap(10),
-                RichText(
-                  text: TextSpan(
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      TextSpan(
-                        text: 'Bergabung ',
-                        style: regular.copyWith(
-                          fontSize: 14,
-                          color: lightActive,
-                        ),
+                      const SizedBox(width: 5),
+                      Text(
+                        'ubah profil',
+                        style: regular.copyWith(fontSize: 16, color: light),
                       ),
-                      TextSpan(
-                        text: DateFormat('dd MMMM yyyy').format(
-                            DateTime.parse(mainController.createdAt.value)),
-                        style: regular.copyWith(
-                          fontSize: 14,
-                          color: dark,
+                      Container(
+                        padding: const EdgeInsets.all(14),
+                        decoration: BoxDecoration(
+                          gradient: primary,
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                      ),
+                        child: SvgPicture.asset(
+                          'assets/icons/pencil.svg',
+                          height: 22,
+                        ),
+                      )
                     ],
                   ),
                 ),
-              ],
-            ),
-            const Gap(35),
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 11),
-                decoration: BoxDecoration(
-                  color: dark,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                width: double.infinity,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const SizedBox(width: 5),
-                    Text(
-                      'ubah profil',
-                      style: regular.copyWith(fontSize: 16, color: light),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(14),
-                      decoration: BoxDecoration(
-                        gradient: primary,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: SvgPicture.asset(
-                        'assets/icons/pencil.svg',
-                        height: 22,
-                      ),
-                    )
-                  ],
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
